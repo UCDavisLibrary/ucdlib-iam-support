8088d4c3fb23c484227f97514f22850997407b9b
Sabrina Baggett <sabaggett@ucdavis.edu>
2023-02-09 00:27:22 -0800

    adding search function to cli

--- cli/bin/ucdlib-iam-person.js
+++ cli/bin/ucdlib-iam-person.js
@@ -5,8 +5,18 @@ const program = new Command();
 program
   .command('search-ucd')
   .description('Search the UC Davis IAM system')
+  .option('-eid, --employeeID <char>|@<file>')
+  .option('-sid, --studentID <char>|@<file>')
+  .option('-n, --name <char>|@<file>')
+  .option('-k, --kerberos <char>|@<file>')
+  .option('-e, --email <char>|@<file>')
+  .option('-f, --first <char>|@<file>','first name', '')
+  .option('-m, --middle <char>|@<file>','middle name', '')
+  .option('-l, --last <char>|@<file>','last name', '')
+  .option('-f, --file <char>|@<file>','file name', 'default.json')
+  .option('-o, --online', 'Query Online Directory', false)
   .action((options) => {
-    people.searchUcd();
+    people.searchUcd(options);
   });
 
   program.parse(process.argv);
