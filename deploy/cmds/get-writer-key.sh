605cc1dea42400d1faa992a7c89c087120b1e949
spelkey-ucd <spelkey@ucdavis.edu>
2023-07-11 15:43:59 -0400

    fix writer key command

--- deploy/cmds/get-writer-key.sh
+++ deploy/cmds/get-writer-key.sh
@@ -1,18 +1,7 @@
 #! /bin/bash
 
 ###
-# download the reader key from the secret manager
-###
-
-set -e
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $SCRIPT_DIR/../..
-
-gcloud secrets versions access latest --secret=itis-iam-support-writer-key > z-delete-me-steve.json
-#! /bin/bash
-
-###
-# download the reader key from the secret manager
+# download the writer key from the secret manager
 ###
 
 set -e
