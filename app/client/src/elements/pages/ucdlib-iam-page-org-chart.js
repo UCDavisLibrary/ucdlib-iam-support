4c1615c6a83d4feab8b2b789f73ffec7fcc98d28
Sabrina Baggett <sabaggett@ucdavis.edu>
2023-11-08 13:27:21 -0800

    stash

---
+++ app/client/src/elements/pages/ucdlib-iam-page-org-chart.js
@@ -0,0 +1,45 @@
+import { LitElement } from 'lit';
+import {render} from "./ucdlib-iam-page-org-chart.tpl.js";
+import "../components/ucdlib-org-chart";
+
+/**
+ * @classdesc Xomponent for displaying the application home page
+ */
+export default class UcdlibIamPageOrgChart extends window.Mixin(LitElement)
+  .with(window.LitCorkUtils) {
+
+  static get properties() {
+    return {
+
+    };
+  }
+
+  constructor() {
+    super();
+    this.render = render.bind(this);
+
+    this._injectModel('AppStateModel');
+  }
+
+  /**
+   * @description Disables the shadowdom
+   * @returns
+   */
+  createRenderRoot() {
+    return this;
+  }
+
+  /**
+   * @method _onAppStateUpdate
+   * @description bound to AppStateModel app-state-update event
+   *
+   * @param {Object} e
+   */
+  async _onAppStateUpdate(e) {
+    if ( e.page != this.id ) return;
+    this.AppStateModel.showLoaded(this.id);
+  }
+
+}
+
+customElements.define('ucdlib-iam-page-org-chart', UcdlibIamPageOrgChart);
---
+++ app/client/src/elements/pages/ucdlib-iam-page-org-chart.tpl.js
@@ -0,0 +1,15 @@
+import { html } from 'lit';
+
+/**
+ * @description Main render function for this element
+ * @returns {TemplateResult}
+ */
+export function render() {
+  return html`
+  <div class="l-3col">
+      <div class='l-second'>
+        <ucdlib-org-chart></ucdlib-org-chart>
+      </div> 
+  </div>
+
+`;}
