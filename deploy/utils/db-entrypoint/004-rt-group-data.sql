5ae27ff356b981e4d2da7f0e7acd5e7a1d145b05
spelkey-ucd <spelkey@ucdavis.edu>
2023-12-14 14:50:19 -0500

    add rt group data #47

---
+++ deploy/utils/db-entrypoint/004-rt-group-data.sql
@@ -0,0 +1,54 @@
+UPDATE groups
+  SET rt_name = 'Access'
+  WHERE id = 10;
+UPDATE groups
+  SET rt_name = 'BMLL'
+  WHERE id = 6;
+UPDATE groups
+  SET rt_name = 'CollStrat'
+  WHERE id = 12;
+UPDATE groups
+  SET rt_name = 'Communications'
+  WHERE id = 2;
+UPDATE groups
+  SET rt_name = 'ContentSupport'
+  WHERE id = 13;
+UPDATE groups
+  SET rt_name = 'Development'
+  WHERE id = 20;
+UPDATE groups
+  SET rt_name = 'DigitalApp'
+  WHERE id = 18;
+UPDATE groups
+  SET rt_name = 'DigitalSchol'
+  WHERE id = 15;
+UPDATE groups
+  SET rt_name = 'Finance'
+  WHERE id = 16;
+UPDATE groups
+  SET rt_name = 'HR'
+  WHERE id = 3;
+UPDATE groups
+  SET rt_name = 'ITIS'
+  WHERE id = 19;
+UPDATE groups
+  SET rt_name = 'MBS'
+  WHERE id = 4;
+UPDATE groups
+  SET rt_name = 'OnlineStrat'
+  WHERE id = 17;
+UPDATE groups
+  SET rt_name = 'Research'
+  WHERE id = 5;
+UPDATE groups
+  SET rt_name = 'ResSvcs'
+  WHERE id = 7;
+UPDATE groups
+  SET rt_name = 'ScholComm'
+  WHERE id = 14;
+UPDATE groups
+  SET rt_name = 'Spec Coll'
+  WHERE id = 11;
+UPDATE groups
+  SET rt_name = 'StudentSvcs'
+  WHERE id = 8;
