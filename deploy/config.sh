73a7b58885b4cb9a92183340d7d649458e4d8b92
spelkey-ucd <spelkey@ucdavis.edu>
2024-07-08 09:55:17 -0400

    increment version

--- deploy/config.sh
+++ deploy/config.sh
@@ -13,7 +13,7 @@ fi
 
 # Main version number we are tagging the app with. Always update
 # this when you cut a new version of the app!
-APP_VERSION=v1.7.0.${BUILD_NUM}
+APP_VERSION=v1.8.0.${BUILD_NUM}
 
 # Repository tags/branchs
 # Tags should always be used for production deployments
