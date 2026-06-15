756040935830fd95c5f7f9dc791fe4386f6e6d84
Sabrina Baggett <sabrinabaggett@administrators-MacBook-Pro.local>
2026-06-15 13:19:23 -0700

    logout fixed

--- services/app/client/src/elements/ucdlib-iam-app.js
+++ services/app/client/src/elements/ucdlib-iam-app.js
@@ -194,7 +194,7 @@ export default class UcdlibIamApp extends Mixin(LitElement)
   const silentCheckSsoRedirectUri = window.location.origin + '/silent-check-sso.html';
 
   // set up listeners keycloak listeners
-  // kc.onAuthRefreshError = () => {AuthModel.logout(true);};
+  kc.onAuthRefreshError = () => {AuthModel.logout(true);};
   kc.onAuthError = () => {AuthModel.redirectUnauthorized();};
   kc.onAuthSuccess = () => {
     customElements.define('ucdlib-iam-app', UcdlibIamApp);
--- services/app/client/src/models/AppStateModel.js
+++ services/app/client/src/models/AppStateModel.js
@@ -12,6 +12,9 @@ class AppStateModelImpl extends AppStateModel {
     this.defaultPage = 'home';
     this.currentPage = this.defaultPage;
     this.store = AppStateStore;
+
+    this.inject('AuthModel');
+
   }
 
   /**
@@ -21,7 +24,7 @@ class AppStateModelImpl extends AppStateModel {
    */
   set(update) {
     if ( update.location.path.length && update.location.path[0] == 'logout' ){
-      this.logout();
+      this.AuthModel.logout();
     }
 
     const modals = document?.querySelectorAll?.('ucdlib-iam-modal');
